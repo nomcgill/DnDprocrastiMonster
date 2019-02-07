d48b69e5761a5a2380516ae9d8a227cdcf026d44
Nolan McGill <nomcgill@gmail.com>
2019-02-06 19:02:54 -0600

    Begin displaying console.log info using assembleInfoOntoPage(), and furthered the STORE image array.

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function gatherRelevantMonsters(theUrlArray, ratingInput) {
         })
     )
     setTimeout(function(){
-        console.log(monsterObjectArray)
+        // console.log(monsterObjectArray)
         var filteredMonsters = []
         for (let i = 1; i < monsterObjectArray.length; i++){
             var singleMonsterResponse = monsterObjectArray[i]
@@ -70,20 +70,23 @@ function gatherRelevantMonsters(theUrlArray, ratingInput) {
         }
         var theSix = shuffleMonsters(filteredMonsters)
         //Now that we've got theSix, what are we going to do with them? (BELOW)
-        console.log(theSix[1].name + ` has an index of ` + theSix[1].index + ` which can be found at ` + theSix[1].url)
-
-
-    }, 3850)
-
+        console.log(theSix[0].name + ` has an index of ` + theSix[0].index + ` which can be found at ` + theSix[0].url)
+        assembleInfoOntoPage(theSix)
+        }, 4150)
 }
 
 //There's only 6, and they shouldn't be the same ones!
 function shuffleMonsters(arrayOfMonsters) {
     // console.log(`shuffleMonsters is happening`)
     var relevantLength = arrayOfMonsters.length
+    console.log(arrayOfMonsters.length + ` total monsters are worthy.`)
     var theSix = []
-    for (let i=0; i<=5; i++){
-        theSix.push(arrayOfMonsters[Math.floor(Math.random() * (relevantLength - 0 + 1))])
+    while (theSix.length < 6){
+        var randomNumber = Math.floor(Math.random() * (relevantLength - 0 + 1))
+        // console.log(randomNumber)
+        if (arrayOfMonsters[randomNumber] != undefined )
+            {theSix.push(arrayOfMonsters[randomNumber])
+        }
     }
     console.log(theSix)
     return theSix
@@ -102,32 +105,51 @@ function filterMonsters(singleMonsterResponse, ratingInput) {
     }
 }
 
-// function displayResults(responseJson) {
-//   console.log(responseJson);
-//   //replace the existing image with the new one
-//   $('.results-img').replaceWith(
-//     `<p class="results-text">${responseJson.message}<p>`
-//   )
+function assembleInfoOntoPage(theSix){
+    console.log(`That includes a ` + theSix[0].name + `.`)
+    if (theSix[0].name == theSix[1].name || theSix[0].name == theSix[2].name || theSix[0].name == theSix[3].name || theSix[0].name == theSix[4].name || theSix[0].name == theSix[5].name){
+        console.log(`Why the duplicates? Because not many foes are that powerful!`)
+    }
+  $('#box1').replaceWith(
+    `<div id="box1" class="boxes">${theSix[0].name}</div>`
+  )
+  $('#box2').replaceWith(
+    `<div id="box2" class="boxes">${theSix[1].name}</div>`
+  )
+  $('#box3').replaceWith(
+    `<div id="box3" class="boxes">${theSix[2].name}</div>`
+  )
+  $('#box4').replaceWith(
+    `<div id="box4" class="boxes">${theSix[3].name}</div>`
+  )
+  $('#box5').replaceWith(
+    `<div id="box5" class="boxes">${theSix[4].name}</div>`
+  )
+  $('#box6').replaceWith(
+    `<div id="box6" class="boxes">${theSix[5].name}</div>`
+  )
 
-//   $('.results').removeClass('hidden');
-// }
+}
 
+// console.log(STORE)
+function singleHomeImage(STORE){
+    var relevantLength = 50
+    // relevantLength = STORE.length WHEN MY STORE ARRAY IS COMPLETE
+    var randomIndex = Math.floor(Math.random() * (relevantLength - 0 + 1))
+    var homeMonsterUrl = STORE[randomIndex]
+    // var homeMonsterUrl = STORE["Ape"]
+    document.getElementById('random').src = homeMonsterUrl
+    document.getElementById('random').alt = homeMonsterUrl
+    document.getElementById('random').title = randomIndex
+    console.log(homeMonsterUrl)
+}
 
-// const photoUrl = 'https://jsonplaceholder.typicode.com/'
-
-// function getImages() { 
-//     const imgArr = [] 
-//     for(let i=1;i<=10;i++){ 
-//         var imgUrl = fetch(photoUrl+'photos/'+i) 
-//         imgArr.push(imgUrl.url) } 
-//         console.log(imgArr) 
-//     }
+singleHomeImage(STORE)
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    loadingIcon()
-        var element = document.getElementById("myDIV")
+    onClickFunction()
      var ratingInput = document.getElementById("challenge-rating").value
     //  console.log(ratingInput)
      checkMonsterList(ratingInput)
@@ -139,18 +161,22 @@ $(function() {
   watchForm();
 });
 
-function loadingIcon(ratingInput){
+function onClickFunction(ratingInput){
     var submitButton = document.getElementById("generate")
     var submitField = document.getElementById("challenge-rating")
     var loadingSwirl = document.getElementById("loading-gif")
+    var homeMonster = document.getElementById("random")
+    var boxes = document.getElementsByClassName("boxes")
+    console.log(boxes)
     submitButton.classList.add("hidden")
     submitField.classList.add("hidden")
     loadingSwirl.classList.remove("hidden")
+    homeMonster.classList.add("hidden")
     setTimeout(function(){
-        // var element = document.getElementById("generate")
+        var element = document.getElementById("generate")
         submitButton.classList.remove("hidden")
         submitField.classList.remove("hidden")
         loadingSwirl.classList.add("hidden")
-    }, 4200)
+    }, 4400)
 }
 
